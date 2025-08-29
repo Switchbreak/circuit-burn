9582904b0b33b548755c70ed6e9bf65b978500f7
Switchbreak <switch@switchbreak.net>
2025-08-29 02:17:19 -0700

    Tire tracks only emit if wheels are in contact

--- script/tire_tracks.gd
+++ script/tire_tracks.gd
@@ -5,19 +5,20 @@ const TIRE_TRACK_LENGTH := 4
 var rate_scale := (self.lifetime / self.amount) / TIRE_TRACK_LENGTH
 
 @onready var _previous_position := global_position
-#@onready var _car := get_parent() as RigidBody3D
+@onready var _wheel_rl := $"../WheelRL"
+@onready var _wheel_rr := $"../WheelRR"
 
 func _process(delta: float) -> void:
-    #if _car.get_contact_count() > 0:
-    var offset := global_position - _previous_position
-    var distance := offset.length()
-    var speed := distance / delta
+    if _wheel_rl.is_in_contact() and _wheel_rr.is_in_contact():
+        var offset := global_position - _previous_position
+        var distance := offset.length()
+        var speed := distance / delta
 
-    if distance > EPSILON:
-        look_at(global_position + offset)
+        if distance > EPSILON:
+            look_at(global_position + offset)
 
-    speed_scale = speed * rate_scale
-    #else:
-        #speed_scale = 0
+        speed_scale = speed * rate_scale
+    else:
+        speed_scale = 0
 
     _previous_position = global_position
